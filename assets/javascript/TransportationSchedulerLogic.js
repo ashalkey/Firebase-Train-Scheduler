c95da62ef8f61dafeec95b8c051ba20719b1aab5
Aldosh <alanshalkey@gmail.com>
2018-07-07 21:52:47 -0600

    initial styling finished, implemented box-shadow on input focus and button hover

--- assets/javascript/TransportationSchedulerLogic.js
+++ assets/javascript/TransportationSchedulerLogic.js
@@ -1,10 +1,10 @@
 var config = {
-    apiKey: "AIzaSyDchSfg2u3rdf5MR769fjt7jN5SVb68mJM",
-    authDomain: "publictransportationscheduler.firebaseapp.com",
-    databaseURL: "https://publictransportationscheduler.firebaseio.com",
-    projectId: "publictransportationscheduler",
-    storageBucket: "publictransportationscheduler.appspot.com",
-    messagingSenderId: "210194199920"
-  };
-  
-  firebase.initializeApp(config);
+  apiKey: "AIzaSyDchSfg2u3rdf5MR769fjt7jN5SVb68mJM",
+  authDomain: "publictransportationscheduler.firebaseapp.com",
+  databaseURL: "https://publictransportationscheduler.firebaseio.com",
+  projectId: "publictransportationscheduler",
+  storageBucket: "publictransportationscheduler.appspot.com",
+  messagingSenderId: "210194199920"
+};
+
+firebase.initializeApp(config);
